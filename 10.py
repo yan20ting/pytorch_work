8db3395c650b220b8262e0e62ae22b2dcd2fc3cf
KadiASD <2106527913@qq.com>
2025-03-20 16:44:39 +0800

    1

---
+++ 10.py
@@ -0,0 +1,17 @@
+import torch
+def test():
+    data1 = torch.randint(0,10,[2,3])
+    data2 = torch.randint(0, 10, [2, 3])
+    print("data1-->",data1)
+    print("data2-->",data2)
+    new0_data = torch.stack([data1,data2],dim=0)
+    print(new0_data.shape)
+    print("new0_data-->",new0_data)
+    new1_data = torch.stack([data1,data2],dim=0)
+    print(new1_data.shape)
+    print("new1_data-->",new1_data)
+    new2_data = torch.stack([data1,data2],dim=0)
+    print(new2_data.shape)
+    print("new2_data-->",new2_data)
+    if __name__ == '__main__':
+        test()
